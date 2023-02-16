c744e75e5718934898a01049be1091c5d2ed2c21
董新 <1447228014@qq.com>
2023-02-16 17:35:00 +0800

    http方式实现wss，调试中

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+	"log"
+	"net/http"
+	"time"
+)
+
+const sk = "c6f8ab07d829850d378f5c88751b249ebe2686a14f17bb80fc2367f4ff3615c3"
+const npub = "npub103kpc6e2cxpsr360h7ap44ae63x3ey5cwn63drmq6jvutucdagfs0c70rg"
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+
+func main()  {
+	//初始化websocket连接
+	var w http.ResponseWriter
+	var r *http.Request
+	// 完成和Client HTTP >>> WebSocket的协议升级
+	WsConn, err := Upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	defer WsConn.Close()
+
+	//1、启动时连接并订阅relayer
+	var relays []*nostr.Relay
+	urls := []string{"ws://110.41.16.146:2700", "ws://182.61.59.53:2700"}
+
+	for _,url := range urls{
+		relay, e := nostr.RelayConnect(context.Background(), url)
+		if e != nil {
+			fmt.Println(e)
+			continue
+		}
+		go Subscribe(relay, context.Background(), WsConn)
+		relays = append(relays, relay)
+	}
+
+
+	//2、监听长连接消息，接收到时发布
+	for {
+		// 接收客户端message
+		_, message, err := WsConn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		//向relayer发布message
+		for _,value := range relays{
+			fmt.Println("60:publish message", string(message))
+			Publish(value, context.Background(), string(message))
+		}
+	}
+}
+
+// Publish 向指定relay连接发送内容为content的事件
+func Publish(r *nostr.Relay, ctx context.Context, content string){
+	pub,_ := nostr.GetPublicKey(sk)
+	ev := nostr.Event{
+		PubKey:    pub,
+		CreatedAt: time.Now(),
+		Kind:      nostr.KindTextNote,
+		Tags:      nil,
+		Content:   content,
+	}
+
+	fmt.Println("published to ", r.Publish(ctx, ev))
+}
+
+func Subscribe(relay *nostr.Relay, c context.Context, conn *websocket.Conn){
+	var filters nostr.Filters
+	if _, v, err := nip19.Decode(npub); err == nil {
+		pub := v.(string)
+		filters = []nostr.Filter{{
+			Kinds:   []int{nostr.KindTextNote},
+			Authors: []string{pub},
+			Limit:   1,
+		}}
+	} else {
+		panic(err)
+	}
+
+	ctx, _ := context.WithCancel(c)
+	sub := relay.Subscribe(ctx, filters)
+	go func() {
+		<-sub.EndOfStoredEvents
+		// handle end of stored events (EOSE, see NIP-15)
+	}()
+
+	for ev := range sub.Events {
+		// 处理监听到的事件（channel将持续开启，直到ctx被cancelled）
+		fmt.Println(ev.ID, "=======", ev.Content)
+		//通过ws向客户端发送message
+		err := conn.WriteMessage(1, []byte(ev.Content))
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
